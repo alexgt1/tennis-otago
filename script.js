7142ec31ff6fd363f58e0f7acdc0e9096817c913
System Administrator <root@Georges-MacBook-Pro.local>
2019-06-27 10:04:51 +1200

    pushing

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-$('.autoplay').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
+$('.variable-width').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  centerPadding: '100px',
+  variableWidth: true,
   autoplay: true,
-  autoplaySpeed: 1000,
+  autoplaySpeed: 2000,
+
 });
